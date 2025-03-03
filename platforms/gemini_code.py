e0aa7c5887f38494edb150b9e15574ec9c102244
Jess Lin <perdendosi@gmail.com>
2025-03-03 14:33:14 -0500

    Update Gemini example

--- platforms/gemini_code.py
+++ platforms/gemini_code.py
@@ -2,31 +2,40 @@ import os
 import rizaio
 import google.generativeai as genai
 
-# requires a valid Google Gemini API key in the GOOGLE_API_KEY env var
-# requires a valid Riza API key in the RIZA_API_KEY env var
+# Get an API key for Gemini from Google and set it as the value of an environment variable named GEMINI_API_KEY
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+
+# Get an API key from https://dashboard.riza.io and set it as the value of an environment variable named RIZA_API_KEY
+riza_client = rizaio.Riza()
+
 
 def execute_python(code:str):
     """ Executes a Python script and returns whatever was printed to stdout.
-    
-    The Python runtime does not have filesystem access, but does include the entire standard library. Make HTTP requests with the httpx or requests libraries. Read input from stdin and write output to stdout.
+
+    The Python runtime does not have filesystem access, but does include the entire standard library. Read input from stdin and write output to stdout.
     """
-    riza = rizaio.Riza()
-    resp = riza.command.exec(
-        language="PYTHON",
+    resp = riza_client.command.exec(
+        language="python",
         code=code
     )
     return resp.stdout
 
+
 def main():
-    genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-    model = genai.GenerativeModel('gemini-1.0-pro-latest', tools=[execute_python])
+    GEMINI_MODEL = "gemini-2.0-flash"
+
+    model = genai.GenerativeModel(
+        GEMINI_MODEL,
+        tools=[execute_python]
+    )
     chat = model.start_chat(enable_automatic_function_calling=True)
-    response = chat.send_message('Please base64 encode the last message I sent you')
+    response = chat.send_message('Please base32 encode this message (use a tool if needed): purple monkey dishwasher')
     print(response.text)
 
     for content in chat.history:
         print(content.role, "->", [type(part).to_dict(part) for part in content.parts])
         print('-'*80)
+
 
 if __name__ == "__main__":
     main()
